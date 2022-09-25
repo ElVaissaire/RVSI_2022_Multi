63bed132562578fd9a9aebdaf509022fd3685ed5
Martine <nogues@et.esiea.fr>
2022-09-25 12:15:14 +0200

    Ajout de la rotation dans les déplacements et ajout des collisions

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -3,48 +3,55 @@ using UnityEngine;
 
 public class PlayerBehavior : NetworkBehaviour
 {
-    [SerializeField] private int        m_speed = 5;
-    [SerializeField] private Vector3    m_deplacement;
+    [SerializeField] private int        m_vitesse = 5;
+    [SerializeField] private Vector3    m_direction;
+    [SerializeField] private Vector3    m_directionTmp;
 
     // Update is called once per frame
     void Update()
     {
         if(IsOwner)
         {
-            m_deplacement = GetDeplacement();
-            if (IsClient && !IsServer)
+            m_directionTmp = GetDirection();
+            if (m_directionTmp != Vector3.zero)
             {
-                EnvoiePositionClientAuServeurServerRpc(m_deplacement);
-            }
-            else
-            {
-                transform.Translate(m_deplacement);
-            }
+                m_direction = m_directionTmp;
+                if (IsClient && !IsServer)
+                    EnvoiePositionRotationClientAuServeurServerRpc(Vector3.forward * m_vitesse * Time.deltaTime, m_direction);
+                else
+                {
+                    transform.Translate(Vector3.forward * m_vitesse * Time.deltaTime); 
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(m_direction), 0.15f);
+                }
+            } 
         }
     }
+    
     [ServerRpc]
-    void EnvoiePositionClientAuServeurServerRpc(Vector3 p_position)
+    void EnvoiePositionRotationClientAuServeurServerRpc(Vector3 p_direction, Vector3 p_rotation)
     {
-        transform.Translate(p_position);
+        transform.Translate(p_direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(p_rotation), 0.15f);
     }
-    Vector3 GetDeplacement()
+    Vector3 GetDirection()
     {
         Vector3 pos = Vector3.zero;
+       
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            pos += Vector3.forward * m_speed * Time.deltaTime;
+            pos += Vector3.forward * m_vitesse * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            pos += Vector3.back * m_speed * Time.deltaTime;
+            pos += Vector3.back * m_vitesse * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            pos += Vector3.left * m_speed * Time.deltaTime;
+            pos += Vector3.left * m_vitesse * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            pos += Vector3.right * m_speed * Time.deltaTime;
+            pos += Vector3.right * m_vitesse * Time.deltaTime;
         }
         return pos;
     }
