2f1bd85fc7f633d08ba06cad7878051f251d97a7
Eléonore Vaissaire <76736017+ElVaissaire@users.noreply.github.com>
2022-09-27 09:51:22 +0200

    gestion cam axe haut bas
    client et serveur + reset le textmesh pro avec l'id du player

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -6,26 +6,24 @@ using TMPro;
 public class PlayerBehavior : NetworkBehaviour
 {
 
-    [SerializeField] private int        m_vitesse = 5;
-    [SerializeField] private int        m_rotSpeed;
-    [SerializeField] private Vector3    m_direction;
-    [SerializeField] private Vector3    m_rotation;
-    [SerializeField] private Vector3    m_rotCam;
-    public NetworkVariable<Vector3>     m_networkRotCam;
-    public NetworkVariable<Quaternion>  m_networkRotationCam;
+    [SerializeField] private int            m_vitesse = 5;
+    [SerializeField] private int            m_rotSpeed;
+    [SerializeField] private Vector3        m_direction;
+    [SerializeField] private Vector3        m_rotation;
+    [SerializeField] private Vector3        m_rotCam;
 
-    [SerializeField] private GameObject m_snowballPrefab;
-    [SerializeField] private Transform  m_snowballSpawn;
+    [SerializeField] private GameObject     m_snowballPrefab;
+    [SerializeField] private Transform      m_snowballSpawn;
 
-    [SerializeField] public Slider      m_healthBar;
-    public NetworkVariable<int>         m_networkVie = new NetworkVariable<int>();
-    public static int                   m_maxVie = 5;
-    [SerializeField] int                m_vie;
+    [SerializeField] public Slider          m_healthBar;
+    public NetworkVariable<int>             m_networkVie = new NetworkVariable<int>();
+    public static int                       m_maxVie = 5;
+    [SerializeField] int                    m_vie;
 
-    private NetworkVariable<int>        m_networkID = new NetworkVariable<int>();
-    public int                          m_ID = -1;
+    private NetworkVariable<int>            m_networkID = new NetworkVariable<int>();
+    public int                              m_ID = -1;
 
-    [SerializeField] private GameObject m_cam;
+    [SerializeField] private GameObject     m_cam;
 
     [SerializeField] public TextMeshProUGUI m_text;
     //public NetworkVariable<string> m_networkText = new NetworkVariable<string>();
@@ -48,18 +46,20 @@ public class PlayerBehavior : NetworkBehaviour
             m_ID = GameManager.Instance.AddPlayer(gameObject);
             m_networkID.Value = m_ID;
             m_networkVie.Value = m_maxVie;
+            //m_networkRotCam.Value = m_rotCam;
             //m_text.text = "Player" + m_ID.ToString();
             //m_text.fontSize = 5;
-
-            print("OnNetworkSpawn " + m_text.text);
         }
         else
         {
             m_ID = m_networkID.Value;
             m_maxVie = m_networkVie.Value;
             GetComponent<MeshRenderer>().material.color = GameManager.Instance.GetColorByID(m_ID);
-            //m_text.text = "Player";
+            //m_rotCam = m_networkRotCam.Value;
         }
+
+        m_text.SetText("Player_" + m_ID);
+        print("OnNetworkSpawn " + m_text.text);
 
         if (!IsOwner)
             m_cam.SetActive(false);
@@ -79,19 +79,33 @@ public class PlayerBehavior : NetworkBehaviour
             m_rotation = GetRotationPersonnage();
             m_rotCam = GetRotationCamera();
 
+            // La rotation de la caméra est faite en locale, pas besoin de la partagée sur le serveur.
+            // Autoriser la rotation entre 330° et 45°
+            m_cam.transform.Rotate(m_rotCam, Space.Self);
+            if(m_cam.transform.localEulerAngles.x > 45.0f && m_cam.transform.localEulerAngles.x < 325.0f)
+            {
+                float rotX;
+
+                if (m_cam.transform.localEulerAngles.x < 180.0f)
+                    rotX = 45.0f;
+                else
+                    rotX = 325.0f;
+
+                m_cam.transform.localEulerAngles = new Vector3(rotX, 0.0f, 0.0f);
+            }
+
+            /*
+            if ((0.0f <= m_cam.transform.eulerAngles.x && m_cam.transform.eulerAngles.x < (45.0f - m_rotCam.x)) ||
+                (((330.0f - m_rotCam.x) < m_cam.transform.eulerAngles.x && m_cam.transform.eulerAngles.x <= 360.0f)))
+            {
+                m_cam.transform.Rotate(m_rotCam);
+            }//*/
+            print(m_cam.transform.eulerAngles);
+
             if (IsServer)
             {
                 transform.Translate(m_direction * m_vitesse * Time.deltaTime);
                 transform.Rotate(m_rotation);
-
-                // Autoriser la rotation entre 330° et 45°
-                if ((0.0f <= m_cam.transform.eulerAngles.x && m_cam.transform.eulerAngles.x < (45.0f - m_rotCam.x) ) || 
-                    (((330.0f - m_rotCam.x) < m_cam.transform.eulerAngles.x && m_cam.transform.eulerAngles.x <= 360.0f)))
-                {
-                    m_cam.transform.Rotate(m_rotCam);
-                }
-                
-                m_networkRotationCam.Value = m_cam.transform.rotation;
             }
             else
             {
@@ -103,16 +117,14 @@ public class PlayerBehavior : NetworkBehaviour
 
         m_vie = m_networkVie.Value;
         m_healthBar.value = m_vie;
-        m_cam.transform.rotation = m_networkRotationCam.Value;
     }
     
     [ServerRpc]
     void EnvoiePositionRotationClientAuServeurServerRpc(Vector3 p_direction, Vector3 p_rotation, Vector3 p_rotCam)
     {
         transform.Translate(p_direction);
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(p_rotation), 0.3f);
         transform.Rotate(p_rotation);
-        //m_cam.transform.Rotate(p_rotCam);
+        
     }
 
     Vector3 GetRotationCamera()     // Tourner uniquement la caméra pour regarder de haut en bas
@@ -131,19 +143,19 @@ public class PlayerBehavior : NetworkBehaviour
     {
         Vector3 pos = Vector3.zero;
        
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.Z))
         {
             pos += Vector3.forward * m_vitesse * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.S))
         {
             pos += Vector3.back * m_vitesse * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.Q))
         {
             pos += Vector3.left * m_vitesse * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
             pos += Vector3.right * m_vitesse * Time.deltaTime;
         }
